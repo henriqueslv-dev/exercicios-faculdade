620d912fbeec2927349783d4680004122d536153
Henrique da Silva de Paula <rique.paula33@gmail.com>
2026-04-28 10:15:30 -0300

    exercicio2.py
    upd exercicio1

---
+++ aula-04/exercicio2.py
@@ -0,0 +1,8 @@
+a = []
+for i in range(10):
+    numero = int(input(f"Digite o {i + 1}º número: "))
+    a.append(numero)
+
+print("Os elementos da lista são:")
+for elemento in a:
+    print(elemento)
